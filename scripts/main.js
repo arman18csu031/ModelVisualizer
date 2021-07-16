25521d49978c7e6fff213881f791317f1a16a5da
arman18csu031 <57385463+arman18csu031@users.noreply.github.com>
2021-07-16 17:17:31 +0530

    fixed some bugs

--- scripts/main.js
+++ scripts/main.js
@@ -21,8 +21,11 @@ function init(){
 
 //Scene Setup
 scene = new THREE.Scene();
-camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 20);
-camera.position.set( - 1.8, 0.6, 2.7 );
+scene.background = new THREE.Color('white');
+
+//Camera Setup
+camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set( 0, 0, 5 );
 
 //Renderer Setup
 renderer = new THREE.WebGLRenderer({ alpha: true });
@@ -34,45 +37,48 @@ renderer.toneMappingExposure = params.exposure;
 
 renderer.outputEncoding = THREE.sRGBEncoding;
 
+renderer.shadowMap.enabled = true;
+renderer.shadowMapType = THREE.PCFSoftShadowMap; //for anti aliasing
+
 //Resize Event
 window.addEventListener("resize", function () {
-    var aspectRatio = window.innerWidth / window.innerHeight;
-    camera.aspect = aspectRatio;
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.updateProjectionMatrix();
+let aspectRatio = window.innerWidth / window.innerHeight;
+camera.aspect = aspectRatio;
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.updateProjectionMatrix();
 });
 
 //Loading Models
 const loader = new GLTFLoader();
 loader.load("assets/mesh/Robot.glb", function (Model) {
-scene.add(Model.scene);
-Model.scene.traverse( function ( child ) {
+    scene.add(Model.scene);
+    Model.scene.position.set(0,-2,0);
+    Model.scene.castShadow = true;
+ });
 
-        if ( child.isMesh ) {
-        
-        mesh = child;
-        scene.add( mesh );
-        
-        }
-        
-} );
-    
- Model.scene.position.set(0,0,0);
-});
+var geo = new THREE.PlaneBufferGeometry(10, 10, 8, 8);
+var mat = new THREE.MeshBasicMaterial({ color: 0x0000FF, side: THREE.DoubleSide });
+var plane = new THREE.Mesh(geo, mat);
+scene.add(plane);
+plane.position.set(0, -2, 0);
+plane.receiveShadow = true;
+plane.rotateX( - Math.PI / 2);
+
 
 //Lightning
 var ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
 scene.add(ambientLight);
 var pointLight = new THREE.PointLight(0xffffff, 1);
-pointLight.position.set(25, 50, 25);
+pointLight.position.set(2, 2, 2);
 scene.add(pointLight);
+pointLight.castShadow = true;
 
 
 //Orbital Controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.addEventListener( 'change', render ); // use if there is no animation loop
-controls.enableZoom = true;
-controls.enablePan = true;
+controls.enableZoom = false;
+controls.enablePan = false;
 controls.target.set( 0, 0, - 0.2 );
 controls.update();
 
@@ -91,40 +97,31 @@ mesh.material.needsUpdate = true;
 render();
 
 } );
-
-//Call to Update function once.
-animate();
-
 updateGUI();
-
 gui.open();
-
 }
 
 function updateGUI() {
-
 if ( guiExposure !== null ) {
-
 gui.remove( guiExposure );
 guiExposure = null;
-
 }
 
 if ( params.toneMapping !== 'None' ) {
-
-guiExposure = gui.add( params, 'exposure', 0, 2 )
-
+guiExposure = gui.add( params, 'exposure', 0.2, 1.8 )
 .onChange( function () {
-
 renderer.toneMappingExposure = params.exposure;
 render();
-
-} );
-
+});
+}
 }
 
+function render() {
+    renderer.render( scene, camera );
 }
 
+//Call to Initializer Function  
+init();
 
 
 
@@ -183,23 +180,4 @@ materialArray.push(new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader()
 Skybox = new THREE.BoxGeometry(10, 10, 10);
 skybox = new THREE.Mesh(Skybox, materialArray);
 scene.add(skybox);*/
-
-
-
-
 //Update Function
-function animate() {
-    var delta = clock.getDelta();
-    if(mixer1)mixer1.update(delta);
-    requestAnimationFrame(animate);
-    render();
-}
-
-function render() {
-
-    renderer.render( scene, camera );
-
-}
-
-//Call to Initializer Function  
-init();
