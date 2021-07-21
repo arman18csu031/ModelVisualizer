91b3890ef9cdd5b6a5eac1a35f162d66d5b66182
arman18csu031 <57385463+arman18csu031@users.noreply.github.com>
2021-07-21 12:47:21 +0530

    lighting-final commit

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,16 @@ import * as THREE from "./three/build/three.module.js";
 import { OrbitControls } from "./three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "./three/examples/jsm/loaders/GLTFLoader.js";
 import { GUI } from "./three/examples/jsm/libs/dat.gui.module.js";
+import { HorizontalBlurShader } from "./three/examples/jsm/shaders/HorizontalBlurShader.js";
+import { VerticalBlurShader } from "./three/examples/jsm/shaders/VerticalBlurShader.js";
 
 let scene, camera, renderer;
-let gui, guiExposure = null;
+let gui;
+const PLANE_HEIGHT = 2.5;
+const PLANE_WIDTH = 2.5;
+const CAMERA_HEIGHT = 0.3;
+let shadowGroup, renderTarget, renderTargetBlur, shadowCamera, cameraHelper, depthMaterial, horizontalBlurMaterial, verticalBlurMaterial;
+let plane, blurPlane, fillPlane;
 
 const params = {
     exposure: 1.0,
@@ -14,6 +21,17 @@ const params = {
 const toneMappingOptions = {
     None: THREE.NoToneMapping,
     ACESFilmic: THREE.ACESFilmicToneMapping
+};
+
+const state = {
+shadow: {
+    blur: 3.5,
+	darkness: 1,
+},
+plane: {
+    color: '#ffffff',
+    opacity: 1,
+}
 };
 
 function init(){
@@ -26,175 +44,192 @@ scene.background = new THREE.Color('white');
 camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.set( 0, 0, 5 );
 
+
+//Loading Models
+const loader = new GLTFLoader();
+loader.load("assets/mesh/Astronaut.glb", function (Model) {
+    const mesh = Model.scene.children[ 0 ];
+    mesh.position.set(0,-2,0);
+    mesh.castShadow = true;
+    mesh.receiveShadow = false;
+    scene.add(mesh);
+ });
+
+shadowGroup = new THREE.Group();
+shadowGroup.position.y = -2.1;
+scene.add( shadowGroup );
+
+renderTarget = new THREE.WebGLRenderTarget( 512, 512 );
+renderTarget.texture.generateMipmaps = false;
+
+renderTargetBlur = new THREE.WebGLRenderTarget( 512, 512 );
+renderTargetBlur.texture.generateMipmaps = false;
+
+const planeGeometry = new THREE.PlaneGeometry( PLANE_WIDTH, PLANE_HEIGHT ).rotateX( Math.PI / 2 );
+const planeMaterial = new THREE.MeshBasicMaterial( {
+        map: renderTarget.texture,
+        opacity: state.shadow.opacity,
+        transparent: true,
+        depthWrite: false,
+} );
+plane = new THREE.Mesh( planeGeometry, planeMaterial );
+plane.renderOrder = 1;
+shadowGroup.add( plane );
+		
+plane.scale.y = - 1;
+			
+blurPlane = new THREE.Mesh( planeGeometry );
+blurPlane.visible = false;
+shadowGroup.add( blurPlane );
+				
+const fillPlaneMaterial = new THREE.MeshBasicMaterial( {
+		color: state.plane.color,
+		opacity: state.plane.opacity,
+		transparent: true,
+		depthWrite: false,
+} );
+fillPlane = new THREE.Mesh( planeGeometry, fillPlaneMaterial );
+fillPlane.rotateX( Math.PI );
+shadowGroup.add( fillPlane );
+
+// the camera to render the depth material from
+shadowCamera = new THREE.OrthographicCamera( - PLANE_WIDTH / 2, PLANE_WIDTH / 2, PLANE_HEIGHT / 2, - PLANE_HEIGHT / 2, 0, CAMERA_HEIGHT );
+shadowCamera.rotation.x = Math.PI / 2; // get the camera to look up
+shadowGroup.add( shadowCamera );
+
+cameraHelper = new THREE.CameraHelper( shadowCamera );
+
+depthMaterial = new THREE.MeshDepthMaterial();
+depthMaterial.userData.darkness = { value: state.shadow.darkness };
+depthMaterial.onBeforeCompile = function ( shader ) {
+
+shader.uniforms.darkness = depthMaterial.userData.darkness;
+shader.fragmentShader = /* glsl */`
+						uniform float darkness;
+						${shader.fragmentShader.replace(
+					'gl_FragColor = vec4( vec3( 1.0 - fragCoordZ ), opacity );',
+					'gl_FragColor = vec4( vec3( 0.0 ), ( 1.0 - fragCoordZ ) * darkness );'
+				)}
+					`;
+};
+
+depthMaterial.depthTest = false;
+depthMaterial.depthWrite = false;
+
+horizontalBlurMaterial = new THREE.ShaderMaterial( HorizontalBlurShader );
+horizontalBlurMaterial.depthTest = false;
+
+verticalBlurMaterial = new THREE.ShaderMaterial( VerticalBlurShader );
+verticalBlurMaterial.depthTest = false;
+
+//Lightning
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
+scene.add(ambientLight);
+
+gui = new GUI();
+const shadowFolder = gui.addFolder( 'shadow' );
+    shadowFolder.open();
+const exposureFolder = gui.addFolder('exposure');
+    exposureFolder.open();
+
+
+shadowFolder.add( state.shadow, 'blur', 0, 15, 0.1 );
+shadowFolder.add( state.shadow, 'darkness', 1, 5, 0.1 ).onChange( function () {
+    depthMaterial.userData.darkness.value = state.shadow.darkness;
+});
+
+exposureFolder.add(params, 'exposure', 0.2, 1.8).onChange( function() {
+    renderer.toneMappingExposure = params.exposure;
+});
+
 //Renderer Setup
-renderer = new THREE.WebGLRenderer({ alpha: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+window.addEventListener( 'resize', onWindowResize );
+document.body.appendChild( renderer.domElement );
 
 renderer.toneMapping = toneMappingOptions[ params.toneMapping ];
 renderer.toneMappingExposure = params.exposure;
 
 renderer.outputEncoding = THREE.sRGBEncoding;
 
-renderer.shadowMap.enabled = true;
-//renderer.shadowMapType = THREE.PCFSoftShadowMap; //for anti aliasing
+new OrbitControls( camera, renderer.domElement );
+
+animate();
+}
 
 //Resize Event
-window.addEventListener("resize", function () {
-let aspectRatio = window.innerWidth / window.innerHeight;
-camera.aspect = aspectRatio;
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.updateProjectionMatrix();
-});
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-//Loading Models
-const loader = new GLTFLoader();
-loader.load("assets/mesh/Robot.glb", function (Model) {
-    scene.add(Model.scene);
-    Model.scene.position.set(0,-2,0);
-    Model.scene.castShadow = true;
- });
- 
-
-var geo = new THREE.PlaneBufferGeometry(10, 10, 8, 8);
-var mat = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, side: THREE.DoubleSide });
-var plane = new THREE.Mesh(geo, mat);
-scene.add(plane);
-plane.position.set(0, -2, 0);
-plane.receiveShadow = true;
-plane.rotateX( - Math.PI / 2);
-
-
-//Lightning
-var ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
-scene.add(ambientLight);
-
-const light = new THREE.DirectionalLight( 0xdfebff, 1 );
-light.position.set( 50, 200, 100 );
-light.position.multiplyScalar( 1.3 );
-
-light.castShadow = true;
-
-light.shadow.mapSize.width = 1024;
-light.shadow.mapSize.height = 1024;
-
-const d = 300;
-
-light.shadow.camera.left = - d;
-light.shadow.camera.right = d;
-light.shadow.camera.top = d;
-light.shadow.camera.bottom = - d;
-
-light.shadow.camera.far = 1000;
-
-scene.add( light );
-
-
-
-//Orbital Controls
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.addEventListener( 'change', render ); // use if there is no animation loop
-controls.enableZoom = false;
-controls.enablePan = false;
-controls.target.set( 0, 0, - 0.2 );
-controls.update();
-
-render();
-
-gui = new GUI();
-
-gui.add( params, 'toneMapping', Object.keys( toneMappingOptions ) )
-
-.onChange( function () {
-
-updateGUI();
-
-renderer.toneMapping = toneMappingOptions[ params.toneMapping ];
-mesh.material.needsUpdate = true;
-render();
-
-} );
-updateGUI();
-gui.open();
+    renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-function updateGUI() {
-if ( guiExposure !== null ) {
-gui.remove( guiExposure );
-guiExposure = null;
+function blurShadow( amount ) {
+
+    blurPlane.visible = true;
+
+    // blur horizontally and draw in the renderTargetBlur
+    blurPlane.material = horizontalBlurMaterial;
+    blurPlane.material.uniforms.tDiffuse.value = renderTarget.texture;
+    horizontalBlurMaterial.uniforms.h.value = amount * 1 / 256;
+
+    renderer.setRenderTarget( renderTargetBlur );
+    renderer.render( blurPlane, shadowCamera );
+
+    // blur vertically and draw in the main renderTarget
+    blurPlane.material = verticalBlurMaterial;
+    blurPlane.material.uniforms.tDiffuse.value = renderTargetBlur.texture;
+    verticalBlurMaterial.uniforms.v.value = amount * 1 / 256;
+
+    renderer.setRenderTarget( renderTarget );
+    renderer.render( blurPlane, shadowCamera );
+
+    blurPlane.visible = false;
+
 }
 
-if ( params.toneMapping !== 'None' ) {
-guiExposure = gui.add( params, 'exposure', 0.2, 1.8 )
-.onChange( function () {
-renderer.toneMappingExposure = params.exposure;
-render();
-});
-}
-}
+function animate( ) {
 
-function render() {
+    requestAnimationFrame( animate );
+   
+    // remove the background
+    const initialBackground = scene.background;
+    scene.background = null;
+
+    // force the depthMaterial to everything
+    cameraHelper.visible = false;
+    scene.overrideMaterial = depthMaterial;
+
+    // render to the render target to get the depths
+    renderer.setRenderTarget( renderTarget );
+    renderer.render( scene, shadowCamera );
+
+    // and reset the override material
+    scene.overrideMaterial = null;
+    cameraHelper.visible = true;
+
+    blurShadow( state.shadow.blur );
+
+    // a second pass to reduce the artifacts
+    // (0.4 is the minimum blur amout so that the artifacts are gone)
+    blurShadow( state.shadow.blur * 0.4 );
+
+    // reset and render the normal scene
+    renderer.setRenderTarget( null );
+    scene.background = initialBackground;
+
     renderer.render( scene, camera );
+    stats.update();
+
 }
 
-//Call to Initializer Function  
 init();
+        
 
 
+        
+            
 
-//********clipping planes********//
-//const globalPlane = new THREE.PlaneGeometry( new THREE.Vector3( -1, 0, 0 ), 0.1 ); //horizontal clipping plane
-
-// ***** Clipping setup (renderer): *****
-/*const globalPlanes = [ globalPlane ],
-Empty = Object.freeze( [] );
-renderer.clippingPlanes = Empty; // GUI sets it to globalPlanes
-renderer.localClippingEnabled = true;
-
-
-// GUI
-
-const gui = new GUI(),
-folderGlobal = gui.addFolder( 'Global Clipping' ),
-propsGlobal = {
-
-    get 'Enabled'() {
-
-        return renderer.clippingPlanes !== Empty;
-
-    },
-    set 'Enabled'( v ) {
-
-       renderer.clippingPlanes = v ? globalPlanes : Empty;
-
-    },
-
-    get 'Plane'() {
-
-        return globalPlane.constant;
-
-    },
-    set 'Plane'( v ) {
-
-        globalPlane.constant = v;
-
-    }
-
-};
-folderGlobal.add( propsGlobal, 'Enabled' );
-folderGlobal.add( propsGlobal, 'Plane', -1, 3 );
-
-//Matterial Array
-let materialArray = [];
-materialArray.push(new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load("assets/skybox/blizzard_ft.jpg"), side: THREE.BackSide }));
-materialArray.push(new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load("assets/skybox/blizzard_bk.jpg"), side: THREE.BackSide }));
-materialArray.push(new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load("assets/skybox/blizzard_up.jpg"), side: THREE.BackSide }));
-materialArray.push(new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load("assets/skybox/blizzard_dn.jpg"), side: THREE.BackSide }));
-materialArray.push(new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load("assets/skybox/blizzard_rt.jpg"), side: THREE.BackSide }));
-materialArray.push(new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load("assets/skybox/blizzard_lf.jpg"), side: THREE.BackSide })); 
-
-//Skybox
-Skybox = new THREE.BoxGeometry(10, 10, 10);
-skybox = new THREE.Mesh(Skybox, materialArray);
-scene.add(skybox);*/
-//Update Function
